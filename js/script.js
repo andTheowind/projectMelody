86a0c11d1ba450680f30319db76a0a76eb5a77e9
andTheowind <59265638+andTheowind@users.noreply.github.com>
2021-08-19 23:50:37 +0300

    Add function "floorColorize"

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 let counterArrowUp = document.querySelector('.counter-arrow-up');
 let counterArrowDown = document.querySelector('.counter-arrow-down');
 let counter = document.querySelector('.counter');
-
+let floor = Array.from(document.querySelectorAll('.home-image > path'))
 
 const counterPlus = () => {
-    let counterPlusOne = Number(counter.textContent) + 1;
-    counter.textContent = counterPlusOne.toString().padStart(2, '0');
+    if(counter.textContent < 17) {
+        let counterPlusOne = Number(counter.textContent) + 1;
+        counter.textContent = counterPlusOne.toString().padStart(2, '0');
+        floorColorize();
+    }
 }
+
 const counterMinus = () => {
     if(counter.textContent > 1) {
         let counterMinusOne = Number(counter.textContent) - 1;
         counter.textContent = counterMinusOne.toString().padStart(2, '0');
+        floorColorize();
     }
 }
 
-
+const floorColorize = () => {
+    floor.map((el) => {
+        el.style.opacity = '0';
+    })
+    let digitFloor = Number(counter.textContent);
+    floor[digitFloor-1].style.opacity = '1';
+}
 counterArrowUp.addEventListener('click', counterPlus);
 counterArrowDown.addEventListener('click', counterMinus);
+
